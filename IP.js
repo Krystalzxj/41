73763ec0d941ffe33c9b7ab5913a53ca430b0c95
Krystalzxj <103553035+Krystalzxj@users.noreply.github.com>
2022-06-15 16:46:08 +0800

    Create IP.js

---
+++ IP.js
@@ -0,0 +1,26 @@
+let url = "http://ip-api.com/json/?lang=en-US"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    let ip = jsonData.query
+  body = {
+    title: "🚀 𝙋𝙍𝙊𝙓𝙔 𝙄𝙉𝙁𝙊  🛰",
+    content: `𝑰𝑷：${ip}\n𝑰𝑺𝑷：${isp}\n𝑳𝑶𝑪: ${emoji} | ${country} - ${city}`,
+    icon: "globe.asia.australia.fill",
+    "icon-color": "#0094c8"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
